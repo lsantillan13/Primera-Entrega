4a56c1227e7759ebbfed1f5b5af40e7acdf20510
Lautaro <lautarosantillann@icloud.com>
2021-10-25 19:47:43 -0300

    25/10

--- Rutas/cartRoutes.js
+++ Rutas/cartRoutes.js
@@ -3,35 +3,60 @@ let cartArr = [];
 import { productsArr } from './productsRoutes.js';
 import fs from 'fs';
 /* */
-
 export class Rutas {
     constructor(id, productos) {}
 
 /*USUARIOS Y ADMINISTRADORES*/
-    agregar(req, res){
-     const params = req.params;
-     const id = params.id;
-     if (productsArr.length === 0)
-     {res.send('<h2>Ooops, it looks like the Cart is quite empty 👀👀</h2>')}
-     else{id == productsArr[id].id ? cartArr.push(productsArr[id]) && res.send({cartArr}) : console.log("Product does not exist");}
-    }
-    
+    // agregar(req, res){
+    //  const params = req.params;
+    //  const id = params.id;
+    //  if (productsArr.length === 0)
+    //  {res.send('<h2>Ooops, it looks like the Cart is quite empty 👀👀</h2>')}
+    //  else{id == productsArr[id].id ? cartArr.push(productsArr[id]) && res.send({cartArr}) : console.log("Product does not exist");}
+    // }
+
     listar(req, res) {
-    let data = fs.readFileSync('./data/cart.txt', 'utf-8');
-    // let parsed = JSON.parse(data)
-     data.length === 0 ? res.json('No hay productos') : res.send({data});
-     fs.appendFile("./data/cart.txt", JSON.stringify(productsArr, 8, "\t"), (err) => {
-      if(err) { console.log(err);}
-      else{console.log("\nFile Contents of file after append:");
-      fs.readFileSync("./data/cart.txt", "utf8")
-    }})};
+        let dataC = fs.readFileSync('./data/cart.txt', 'utf-8');
+        let jsonC = JSON.parse(dataC);
+            /**/
+              if (jsonC.length != 0)
+               {res.json(jsonC)}
+              else if(cartArr.length === 0)
+               {res.json('No hay productos')}
+              else
+               {res.json(cartArr)}
+            };
+        
+
+    agregar(req, res){
+    let dataC = fs.readFileSync('./data/cart.txt', 'utf-8');
+    let dataP = fs.readFileSync('./data/products.txt', 'utf-8');
+    let jsonC = JSON.parse(dataC);
+    let jsonP = JSON.parse(dataP);
+        const params = req.params;
+        const id = params.id;
+        const i = id;
+        console.log(jsonP[id])
+
+        id == jsonP[i].id ? cartArr.push({...jsonP[id]}) && res.send(cartArr) : res.send(`Product id ${id} doesn't exist`)
+        
+        fs.writeFileSync("./data/cart.txt", JSON.stringify(cartArr), (err) => {
+        if(err)
+         {console.log(err);}
+        else
+         {console.log("\nFile Contents of file after append:")}
+        })
+    }
 
     listarId(req, res) {
      let params = req.params;
      let id = params.id;
      let i = id;
-     id == cartArr[i].id ? res.json(cartArr[i]) : res.json({ Error: 'Producto no encontrado' });
+     let encontrar = parsedData.find(e => e.id == id);
+     console.log(encontrar)
+     id = encontrar ? res.json(encontrar) : res.json({ Error: 'Producto no encontrado' });
     }
+
 
     // guardar(req, res) {
     //  let { title, price, thumbnail } = req.body;
@@ -41,11 +66,13 @@ export class Rutas {
     // };
 
     borrar(req, res) {
+        const dataC = fs.readFileSync('./data/cart.txt', 'utf-8');
+        const jsonC = JSON.parse(dataC);
      let params = req.params;
      let id = params.id;
-     const encontrar = cartArr.find(e => e.id == id);
+     const encontrar = jsonC.find(e => e.id == id);
       if (encontrar === undefined) {res.send({ msj: "El producto no existe" })}
-      else {cartArr = cartArr.filter(e => e.id != id);
+      else {cartArr = jsonC.filter(e => e.id !== id);
      res.send(encontrar);}
     };
     
--- Rutas/productsRoutes.js
+++ Rutas/productsRoutes.js
@@ -3,69 +3,94 @@ import fs from 'fs';
 export let productsArr = [];
 let id = 0;
 
-const arrTxt = fs.readFileSync('./data/products.txt', 'utf-8');
-let parsed  = JSON.parse(arrTxt);
-
 /*CLASS METHODS*/
 export class Rutas {
     constructor(id, productos) {}
-
-
-//                          //
-/*USUARIOS Y ADMINISTRADORES*/
-//                          //
-
-    listar(req, res) {
-     parsed.length === 0 ? res.json('No hay productos') : res.json(parsed);
-    
-    };
-
-//
-    listarId(req, res) {
-     let params = req.params;
-     let id = params.id;
-     let i = id;
-     id == productsArr[i].id ? res.json(productsArr[i]) : res.json({ Error: 'Producto no encontrado' })
-    };
-
-//
-//                  //
-  /*ADMINISTRADORES*/
-//                  //
-//
     /*agregar / guardar*/
     guardar(req, res) {
      let { title, price, thumbnail, stock, description, timestamp, code, quantity } = req.body;
      let newProduct = { title, price, thumbnail, stock, description, timestamp, code, id: id++, quantity: 1 };
-     productsArr.push(newProduct);
-     res.send(newProduct);
-
-
-     fs.writeFileSync("./data/products.txt", JSON.stringify(productsArr), (err) => {
-        if(err) {console.log(err);}
-        else{console.log("\nFile Contents of file after append:")}})
+      productsArr.push(newProduct); res.send(newProduct);
+        /**/
+         fs.writeFileSync("./data/products.txt", JSON.stringify(productsArr), (err) => {
+        if(err)
+         {console.log(err);}
+        else
+         {console.log("\nFile Contents of file after append:")}})
     };
-//
+
+    listar(req, res) {
+    let data = fs.readFileSync('./data/products.txt', 'utf-8');
+    let parsed  = JSON.parse(data);
+    /**/
+      if (parsed.length != 0)
+       {res.json(parsed)}
+      else if(productsArr.length === 0)
+       {res.json('No hay productos')}
+      else
+       {res.json(parsed)}
+    };
+
+    listarId(req,res){
+        let data = fs.readFileSync('./data/products.txt', 'utf-8');
+        let parsed  = JSON.parse(data);
+        let params = req.params;
+        let id = params.id;
+        let i= id;
+        id == parsed[i].id ?  res.json(parsed[i]) : res.json({Error: 'Producto no encontrado'})
+    }
+
     actualizar(req, res) {
+        /*Leer txt*/
+     let data = fs.readFileSync('./data/products.txt', 'utf-8');
+     let parsed  = JSON.parse(data);
      let body = req.body;
      let {id} = req.params;
-     const buscar = productsArr.find(producto => producto.id == id)
+        /*Buscar producto*/
+     const buscar = parsed.find(producto => producto.id == id)
+        /*Si no existe*/
      if (buscar === undefined) {res.send({msj: "El producto no existe"})}
      else {
         buscar["title"] = body["title"];
         buscar["price"] = body["price"];
         buscar["thumbnail"] = body["thumbnail"];
-        res.send({Msj: 'Producto actualizado', producto: productsArr[id] })}
+        res.send({Msj: 'Producto actualizado', producto: parsed[id] })}
+        /**/
+        productsArr.push(parsed);
+        /**/
+        fs.writeFileSync("./data/products.txt", JSON.stringify(parsed), (err) => {
+            if(err)
+             {console.log(err);}
+            else
+             {console.log("\nFile Contents of file after append:")}})
     };
 //
+    // borrar(req, res) {
+    //     let { id } = req.params;
+    // const data = fs.readFileSync('./data/products.txt', 'utf-8');
+    //     let parsed  = JSON.parse(data);
+    // const buscar = parsed.find(producto => producto.id == id);
+    // console.log(buscar)
+     
+    //  if (buscar === undefined) {res.send({ msj: "El producto no existe" })}
+    //  else {parsed = productsArr.filter(producto => producto.id != id); res.send(buscar)}
+
+    // //  fs.writeFileSync("./data/products.txt", JSON.stringify(productsArr), (err) => {
+    // //     if(err) {console.log(err);}
+    // //     else{console.log("\nFile Contents of file after append:")}})
+    
+
     borrar(req, res) {
-     let { id } = req.params;
-     const buscar = productsArr.find(producto => producto.id == id)
-     if (buscar === undefined) {res.send({ msj: "El producto no existe" })}
-     else {productsArr = productsArr.filter(producto => producto.id != id); res.send(buscar);}
-    };
-//
-/*
-*/
-//
+    let data = fs.readFileSync('./data/products.txt', 'utf-8');
+    let parsed  = JSON.parse(data);
+    let params = req.params;
+    let id = params.id;
+    let encontrar = parsed.find(e => e.id == id);
+      //
+         if (encontrar === undefined)
+         {res.send({ msj: "El producto no existe" })}
+         else
+         {parsed = parsed.filter(e => e.id != id); res.send(encontrar);}
+         console.log(encontrar);
+    }
 };
